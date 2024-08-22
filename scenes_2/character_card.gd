64f5d8762fc73a4eb1d568b69ed94739221e81d9
tH0m4Ssxsqrd <thomasqsa@gmail.com>
2024-08-22 12:24:59 -0300

    Adiciona QuantityBar

--- scenes_2/character_card.gd
+++ scenes_2/character_card.gd
@@ -3,10 +3,10 @@ extends Control
 # Declare variables for the components
 @onready var texture_rect = $Vertical/TextureRect
 @onready var name_label = $Vertical/NameLabel
-@onready var hp_label = $Vertical/StatsContainer/HPLabel
-@onready var mana_label = $Vertical/StatsContainer/ManaLabel
-@onready var force_label = $Vertical/StatsContainer/ForceLabel
-@onready var int_label = $Vertical/StatsContainer/IntLabel
+@onready var hp_label = $Vertical/StatsContainer/HpBar
+@onready var mana_label = $Vertical/StatsContainer/ManaBar
+@onready var force_label = $Vertical/StatsContainer/ForceBar
+@onready var int_label = $Vertical/StatsContainer/IntBar
 
 var images = [
 	"res://scenes_2/character_cards/52921841_p0.png",
@@ -25,10 +25,16 @@ func set_character():
 	texture_rect.texture = chosen_image
 
 	name_label.text = generate_random_name()
-	hp_label.text = "HP: %d" % randi_range(50, 100)
-	mana_label.text = "Mana: %d" % randi_range(20, 80)
-	force_label.text = "Force: %d" % randi_range(10, 50)
-	int_label.text = "Intelligence: %d" % randi_range(10, 50)
+	
+	hp_label.set_label("HP")
+	mana_label.set_label("Mana Points")
+	force_label.set_label("Force")
+	int_label.set_label("Intelligence")
+	
+	hp_label.set_value(randi_range(50, 100))
+	mana_label.set_value(randi_range(20, 80))
+	force_label.set_value(randi_range(10, 50))
+	int_label.set_value(randi_range(10, 50))
 
 func generate_random_name() -> String:
 	var names = ["Aragorn", "Legolas", "Gimli", "Gandalf", "Frodo", "Samwise"]
